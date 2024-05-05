2f7f416aafb727b181362ae4a6fab8611c9032aa
moiduy04 <moiduy04@gmail.com>
2024-05-05 16:22:00 +0700

    Change to working code

--- src/main/java/CalculatorBMI.java
+++ src/main/java/CalculatorBMI.java
@@ -1,16 +1,15 @@
 public class CalculatorBMI {
-//  Pass all tests
   private static final String[] output = {"Thiếu cân", "Không thiếu cân"};
   public static String calculate(double weight, double height) {
-//    if (weight <= 0) {
-//      throw new IllegalArgumentException("Cân nặng nhỏ hơn 0");
-//    } else if (height <= 0) {
-//      throw new IllegalArgumentException("Chiều cao nhỏ hơn 0");
-//    } else if (1000 <= weight) {
-//      throw new IllegalArgumentException("Cân nặng quá lớn");
-//    } else if (1000 <= height) {
-//      throw new IllegalArgumentException("Chiều cao quá lớn");
-//    }
+    if (weight <= 0) {
+      throw new IllegalArgumentException("Cân nặng nhỏ hơn 0");
+    } else if (height <= 0) {
+      throw new IllegalArgumentException("Chiều cao nhỏ hơn 0");
+    } else if (1000 <= weight) {
+      throw new IllegalArgumentException("Cân nặng quá lớn");
+    } else if (1000 <= height) {
+      throw new IllegalArgumentException("Chiều cao quá lớn");
+    }
 
     double BMI = weight / (height * height);
     BMI = (double) Math.round(BMI * 10) / 10;
